d701aba1829dcd976182ecc37bf4bd3bbbd0df65
HazardousBackup <135185205+HazardousBackup@users.noreply.github.com>
2024-01-02 11:55:52 -0600

    Implementation of Raw GOG Parser

--- src/lib/parsers/raw-gog.parser.ts
+++ src/lib/parsers/raw-gog.parser.ts
@@ -1,10 +1,8 @@
 import { ParserInfo, GenericParser, ParsedData } from '../../models';
 import { APP } from '../../variables';
 import * as _ from "lodash";
-import * as fs from "fs-extra";
-import * as genericParser from '@node-steam/vdf';
-import * as path from "path";
-import { stat } from 'original-fs';
+import * as os from "os";
+import Registry from "winreg";
 
 export class RawGOGParser implements GenericParser {
 
@@ -18,9 +16,70 @@ export class RawGOGParser implements GenericParser {
       };
   }
 
+  private processRegKey(regkey: Registry.Registry){
+    return new Promise<ParsedData["success"][number]>((resolve, reject) => {
+      regkey.values((err: Error, values: Registry.RegistryItem[]) => {
+        if (err) {
+          return reject(err);
+        }
+        if (values) {
+          let entry: ParsedData["success"][number] = {
+            extractedTitle: values.find((entry) => entry.name === 'gameName').value,
+            filePath: values.find((entry) => entry.name === 'launchCommand').value,
+            extractedAppId: values.find((entry) => entry.name === 'gameID').value,
+            launchOptions: (values.find((entry) => entry.name === 'launchParam') ?? {value: ''}).value,
+            startInDirectory: values.find((entry) => entry.name === 'workingDir').value
+          }
+          return resolve(entry);
+        }
+      });
+    });
+  }
+
+  private getRegInstalled(){
+    return new Promise<ParsedData["success"]>((resolve, reject) => {
+      const rootkey: string = '\\SOFTWARE\\WOW6432Node\\GOG.com\\Games\\'
+      const reg = new Registry({
+        hive: Registry.HKLM,
+        key: rootkey,
+      });
+
+      reg.keys((err: Error, regkeys: Registry.Registry[]) => {
+        if (err) {
+          return reject(err);
+        }
+        if (regkeys) {
+          const promiseArr = regkeys.map((regkey) => this.processRegKey(regkey))
+          Promise.all(promiseArr).then((parsedArray) => {
+            return resolve(parsedArray);
+          }).catch((err2) => {
+            return reject(err2)
+          });
+        } else {
+          return resolve([]);
+        }
+      });
+    });
+  }
+
   execute(directories: string[], inputs: { [key: string]: any }, cache?: { [key: string]: any }) {
     return new Promise<ParsedData>((resolve,reject)=>{
-        reject();
-    })
+      
+      let parsePromise: Promise<ParsedData["success"]>;
+
+      if(os.type()=='Windows_NT') {
+        parsePromise = this.getRegInstalled();
+      } else {
+        return reject(this.lang.errors.gogNotCompatible);
+      }
+
+      parsePromise.then((games) => {
+        let parsedData: ParsedData = { success: games, failed: [] };
+        resolve(parsedData);
+      }).catch((err)=>{
+        reject(this.lang.errors.fatalError__i.interpolate({error: err}));
+      });
+
+    });
   }
 }
